209f0f0d23a7befae59dba18415a3bdb97568fef
Ahmed Nadar <ahmedfay@hotmail.com>
2023-05-03 20:54:53 -0400

    Add emoji icons for status and show that in project index and show views

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,16 @@
 module ProjectsHelper
+  def status_emoji(status)
+    case status.to_sym
+    when :pending
+      '📝'
+    when :in_progress
+      '🚧'
+    when :completed
+      '✅'
+    when :cancelled
+      '📦'
+    else
+      '❓'
+    end
+  end
 end
